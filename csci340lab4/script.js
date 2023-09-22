07b9908efac55e9c436f3ee8552cfb032ee132c4
tmoslander <bmoslander0624@gmail.com>
2023-09-22 16:05:16 -0500

    Fixed buttons and json requests(yay office hours!)

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    $('clicker1').click(function() {
+    $('#clicker1').click(function() {
+        console.log("Cat button!");
         $.ajax({
             dataType: "json",
             url: "https://cataas.com/cat?json=true",
             success: function(results) {
-                console.log(results["url"]);
+                console.log(results);
                 if (results["url"].endsWith(".mp4")) {
                     $('#cat').attr("src", "images/blank.png");
                 } else {
-                    $('#cat').attr("src", results["url"]);
+                    $('#cat').attr("src", "https://cataas.com/" + results["url"]);
                 }
             },
             error: function(xhr,status,error) {
@@ -16,22 +17,20 @@ $(document).ready(function() {
             }
         })
     });
-    $('clicker2').click(function() {
-
-        $.ajax({
-            dataType: "json",
-            url: "https://api.pokemontcg.io/v2/cards",
-            success: function(results) {
-                console.log(results["url"]);
-                if (results["url"].endsWith(".mp4")) {
-                    $('#card').attr("src", "images/blank.png");
-                } else {
-                    $('#card').attr("src", results["url"]);
-                }
-            },
-            error: function(xhr,status,error) {
-                console.log(error);
-            }
-        })
+    $.ajax({
+        dataType: "json",
+        url: "https://api.pokemontcg.io/v2/cards",
+        success: function(results) {
+            console.log(results);
+            cards = results;
+            $('#card').attr("src", results["data"][0]["images"]["large"]);
+        },
+        error: function(xhr,status,error) {
+            console.log(error);
+        }
+    });
+    $('#clicker2').click(function() {
+        console.log("Card button!");
+        $('#card').attr("src", cards["data"][4]["images"]["large"]);
     });
 });
